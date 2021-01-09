61178bf8d6158fcac740691e7823e7c4b1fa0c91
vugutsa <vugutsamercy84@gmail.com>
2021-01-09 23:14:21 +0300

    add the class location to the models

--- image/models.py
+++ image/models.py
@@ -7,9 +7,36 @@ class Image(models.Model):
     location = models.ForeignKey(Location, on_delete = models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    article_image = models.ImageField(upload_to = '/')
     email = models.EmailField()
     
+    def save_image(self):
+        self.save()
     
-#  class Location(models.Model): 
-#     name = models.CharField(max_length=60)   
+    def delete_image(self):
+        self.dele
+        
+        @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+    
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id=id).all()
+        return image  
+    
+      
+ class Location(models.Model): 
+    name = models.CharField(max_length=60)   
+
+    def save_location(self):
+        self.save()
+        
+    def delete_location(self):
+        self.delete()
+        
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(name = value)
+        
+    def __str__(self):
+        return self.name 
