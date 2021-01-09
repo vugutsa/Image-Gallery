295a8a35d3cbb955907801228683d8fe428cdf63
vugutsa <vugutsamercy84@gmail.com>
2021-01-09 23:05:06 +0300

    add the class image to the model

--- image/models.py
+++ image/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 # Create your models here.
-# class Image(models.Model):
-#     first_name = models.CharField(max_length =30)
-#     last_name = models.CharField(max_length =30)
-#     email = models.EmailField()
+class Image(models.Model):
+    name = models.CharField(max_length =30)
+    description = models.TextField()
+    location = models.ForeignKey(Location, on_delete = models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    article_image = models.ImageField(upload_to = '/')
+    email = models.EmailField()
+    
+    
+#  class Location(models.Model): 
+#     name = models.CharField(max_length=60)   
