6fe10e9eb63917ca6d62739f3e3e3b7a884f42d5
vugutsa <vugutsamercy84@gmail.com>
2021-01-09 21:03:23 +0300

    import the datetime module and create an image view function

--- image/views.py
+++ image/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-
+import datetime as dt
 # Create your views here.
 
 def welcome(request):
     return HttpResponse('Hey you! Welcome to my Image-Gallery')
+
+def image(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
